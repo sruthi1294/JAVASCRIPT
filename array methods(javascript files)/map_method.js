4a6eff05b9026f5184c7d04c7f4591063759d5cb
sruthi <41191158+sruthi1294@users.noreply.github.com>
2018-10-26 21:57:47 +0530

    Update map_method.js

--- array methods(javascript files)/map_method.js
+++ array methods(javascript files)/map_method.js
@@ -1,9 +1,7 @@
-<script>
+
 var plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
 
 document.write(plants.pop() +"<br/ >");
 document.write((plants) + "<br/ >");
 plants.pop();
 document.write(plants);
-
-</script>
