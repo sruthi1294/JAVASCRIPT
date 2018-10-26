81147c4c99f8212a17549432422e39c17d8adbf2
sruthi <41191158+sruthi1294@users.noreply.github.com>
2018-10-26 22:13:10 +0530

    Update reduce_method.js

--- array methods(javascript files)/reduce_method.js
+++ array methods(javascript files)/reduce_method.js
@@ -1,5 +1,10 @@
-<script>
-var fruits = ["Banana", "Orange", "Apple"]
-document.write(fruits.push("Kiwi") + "<br/ >");
-document.write(fruits);
-</script>
+var numbers = [65, 44, 12, 4];
+
+function getSum(total, num) 
+{
+    return total + num;
+}
+function myFunction(item) 
+{
+    document.getElementById("demo").innerHTML = numbers.reduce(getSum);
+}
